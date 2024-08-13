fd97491acafef882dda6792d83ba736e33dfcdca
Pablo Matias Ochoa <158297956+codeOchoa@users.noreply.github.com>
2024-08-13 10:11:13 -0300

    Construccion de estructura de sitio

--- src/_app.js
+++ src/_app.js
@@ -1,1 +1,15 @@
 // Rutas
+
+import React from 'react';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      {/* Content of your page */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/_footer.js
@@ -0,0 +1,16 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
